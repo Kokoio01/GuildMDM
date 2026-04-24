29a0d83c13c3d695932750fe5f9f0dc0e78a4814
Lukas <210173666+Kokoio01@users.noreply.github.com>
2026-04-24 16:02:57 +0200

    feat: basic db schema

--- src/db/migrations/001_initial.sql
+++ src/db/migrations/001_initial.sql
@@ -1,4 +1,27 @@
-CREATE TABLE IF NOT EXISTS test (
-  id int PRIMARY KEY,
-  test int NOT NULL
+CREATE TABLE IF NOT EXISTS networks (
+  id SERIAL PRIMARY KEY,
+  joinKey varchar(8) UNIQUE, -- NanoID
+  masterGuildId BIGINT UNIQUE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS nodes (
+  id SERIAL PRIMARY KEY,
+  guildId BIGINT UNIQUE NOT NULL,
+  networkId int NOT NULL,
+  CONSTRAINT fk_network_nodes
+    FOREIGN KEY (networkId)
+    REFERENCES networks(id)
+    ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS policies (
+    id SERIAL PRIMARY KEY,
+    networkId int NOT NULL,
+    name varchar(255),
+    type varchar(255) NOT NULL,
+    config JSONB,
+    CONSTRAINT fk_network_policies
+      FOREIGN KEY (networkId)
+      REFERENCES networks(id)
+      ON DELETE CASCADE
 );
